3dc52f5a796355d3c55cbc6b6015a12bf1978ef1
g0t0wasd <g0t0@bigmir.net>
2015-02-16 15:47:23 +0200

    Snake game. Explanations at http://pythonicway.com/index.php/python-games/19-python-arcade-games/18-python-snake

--- games/arcade_games/snake.py
+++ games/arcade_games/snake.py
@@ -1,13 +1,16 @@
 from tkinter import *
 import random
 
+# Globals
 WIDTH = 800
 HEIGHT = 600
 SEG_SIZE = 20
 IN_GAME = True
 
 
+# Helper functions
 def create_block():
+    """ Creates an apple to be eaten """
     global BLOCK
     posx = SEG_SIZE * random.randint(1, (WIDTH-SEG_SIZE) / SEG_SIZE)
     posy = SEG_SIZE * random.randint(1, (HEIGHT-SEG_SIZE) / SEG_SIZE)
@@ -17,22 +20,27 @@ def create_block():
 
 
 def main():
+    """ Handles game process """
     global IN_GAME
     if IN_GAME:
         s.move()
         head_coords = c.coords(s.segments[-1].instance)
         x1, y1, x2, y2 = head_coords
+        # Check for collision with gamefield edges
         if x2 > WIDTH or x1 < 0 or y1 < 0 or y2 > HEIGHT:
             IN_GAME = False
+        # Eating apples
         elif head_coords == c.coords(BLOCK):
             s.add_segment()
             c.delete(BLOCK)
             create_block()
+        # Self-eating
         else:
             for index in range(len(s.segments)-1):
                 if head_coords == c.coords(s.segments[index].instance):
                     IN_GAME = False
         root.after(100, main)
+    # Not IN_GAME -> stop game and print message
     else:
         c.create_text(WIDTH/2, HEIGHT/2,
                       text="GAME OVER!",
@@ -82,17 +90,21 @@ class Snake(object):
         if event.keysym in self.mapping:
             self.vector = self.mapping[event.keysym]
 
+# Setting up window
 root = Tk()
 root.title("PythonicWay Snake")
 
 
 c = Canvas(root, width=WIDTH, height=HEIGHT, bg="#003300")
 c.grid()
+# catch keypressing
 c.focus_set()
+# creating segments and snake
 segments = [Segment(SEG_SIZE, SEG_SIZE),
             Segment(SEG_SIZE*2, SEG_SIZE),
             Segment(SEG_SIZE*3, SEG_SIZE)]
 s = Snake(segments)
+# Reaction on keypress
 c.bind("<KeyPress>", s.change_direction)
 
 create_block()
